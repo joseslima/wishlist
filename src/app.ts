8071400c08bc3a6bc7acde6171135d96bd2dd0cd
joseslima <guislima47@gmail.com>
2022-01-08 21:11:18 -0300

    Swagger implementation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 
 import { createConnection } from 'typeorm'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger/openapi.json'
 
 export class App {
   private port: number | string
 
   constructor() {
-    this.port =  process.env.PORT || 80
+    this.port = process.env.PORT || 80
   }
 
   public async start() {
@@ -19,6 +21,8 @@ export class App {
     app.set('env', process.env.APP_ENV)
 
     app.use(AppRouter)
+
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
     app.listen(this.port, () => {
       console.log('App is listening on port ', this.port)
